8cbb586636ef1bd283a4f88eb67094c84c03f900
kvbxss <kubadownar444@gmail.com>
2026-01-22 14:57:03 +0100

    chore: update Dockerfile for improved deployment and settings
    - Added system-level dependencies for numpy, scipy, pandas, and sklearn to the Dockerfile.
    - Modified the command to run the application using gunicorn and included a collectstatic step for static files.
    - Updated the DATABASE_URL setting in core/settings.py to use a string placeholder for better clarity.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -99,7 +99,7 @@ if DATABASE_URL:
         import dj_database_url
         DATABASES = {
             'default': dj_database_url.config(
-                default=DATABASE_URL,
+                default='DATABASE_URL',
                 conn_max_age=600,
                 conn_health_checks=True,
             )
