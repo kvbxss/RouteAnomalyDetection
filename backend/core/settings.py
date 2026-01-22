9d61cbd1427e5cf942484e42fd071090a94f7b0c
kvbxss <kubadownar444@gmail.com>
2026-01-22 23:39:00 +0100

    chore: add log directory creation in settings
    - Introduced a LOG_DIR variable to define the logs directory path.
    - Implemented a check to create the logs directory if it does not exist, ensuring proper logging setup for the application.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -16,6 +16,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
